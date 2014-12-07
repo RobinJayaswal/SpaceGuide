fe2ff78b7553b9eb1afd5be40e36ba1eec693dbc
Robin Jayaswal <robin.e.jayaswal.18@dartmouth.edu>
2014-12-06 19:26:34 -0800

    Initial Commit

---
+++ Outer Space Guide/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Outer Space Guide
+//
+//  Created by RobinJayaswal on 12/6/14.
+//  Copyright (c) 2014 RobinJayaswal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Outer Space GuideTests/Outer_Space_GuideTests.m
@@ -0,0 +1,40 @@
+//
+//  Outer_Space_GuideTests.m
+//  Outer Space GuideTests
+//
+//  Created by RobinJayaswal on 12/6/14.
+//  Copyright (c) 2014 RobinJayaswal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Outer_Space_GuideTests : XCTestCase
+
+@end
+
+@implementation Outer_Space_GuideTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
